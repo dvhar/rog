3a24c073bb28a7f9f1b0b7f438684ed8e92cb3fe
dvhar <dh@protonmail.com>
2023-09-04 01:26:45 -0500

    add socket ipc, need to fix blocking problem

--- rog.cc
+++ rog.cc
@@ -1,8 +1,19 @@
 #include <array>
+#include <thread>
 #include <iostream>
 #include <algorithm>
 #include <stdexcept>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
+#define PORT 4433
+#define CHECK(A,B) if ((A)<0){ perror(B); exit(1); }
 
 class ringbuf {
 	static const unsigned long bufsize = 1024;
@@ -14,7 +25,7 @@ class ringbuf {
 	ringbuf(){
 		fill(buf.begin(), buf.end(), 0);
 	}
-	void write(string_view data){
+	void in(string_view data){
 		if (data.size() == 0)
 			return;
 		hasdata = true;
@@ -35,7 +46,7 @@ class ringbuf {
 			rear = nextfront;
 		front = nextfront;
 	}
-	int read(string& out){
+	int out(string& out){
 		out.clear();
 		if (!hasdata)
 			return 0;
@@ -51,7 +62,81 @@ class ringbuf {
 	}
 };
 
-int main(int argc, char** argv){
+void sockserve(ringbuf& rb);
+void readinput(){
+	char buf[1024];
+	int len;
+	string out;
 	ringbuf rb{};
+	thread t([&](){ sockserve(rb); });
+	while (1){
+		len = read(0, buf, sizeof(buf));
+		rb.in(string_view(buf, len));
+	}
+	t.join();
+}
+
+void sockserve(ringbuf& rb){
+	int sockfd;
+    struct sockaddr_in serv_addr;
+    char buffer[256];
+    int n;
+    
+    CHECK(sockfd = socket(AF_INET, SOCK_STREAM, 0), "ERROR opening socket");
+	serv_addr = {.sin_family=AF_INET, .sin_port=htons(PORT), .sin_addr={.s_addr=INADDR_ANY}};
+    bzero(&(serv_addr.sin_zero), 8);
+    CHECK(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)), ("ERROR on binding"));
+    listen(sockfd,5);
+    while(1){
+        int new_socket;
+        struct sockaddr_in cli_addr;
+        socklen_t clilen = sizeof(cli_addr);
+        CHECK(new_socket = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen), "ERROR on accept");
+		if ((n = read(new_socket, buffer, 255)) != 0) {
+			buffer[n] = 0;
+			printf("Received: %s\n", buffer);
+			while (1){
+				string log;
+				if (!rb.out(log)){
+					sleep(1);
+					continue;
+				}
+				CHECK(sendto(new_socket, log.data(), log.size(), 0, (struct sockaddr *)&cli_addr, sizeof(cli_addr)), "Error sending data to client\n");
+				printf("wrote %d\n", n);
+			}
+		}
+		close(new_socket);
+	}
+}
+
+
+void sockclient(){
+	int sockfd;
+    struct sockaddr_in serv_addr;
+    char buffer[1024];
+    int n;
+
+    CHECK(sockfd = socket(AF_INET, SOCK_STREAM, 0), "Error creating socket\n");
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    CHECK(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)), "Error connecting to server\n");
+    strcpy(buffer, "Hello from client!");
+    CHECK(n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)), "Error sending data to server\n");
+	while ((n = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL)) != 0){
+		CHECK(n, "Error receiving data from server\n");
+		buffer[n] = 0;
+		printf("Received: %s\n", buffer);
+	}
+	printf("recieved %d, closing connection\n", n);
+    close(sockfd);
+}
+
+int main(int argc, char** argv){
+	if (argc < 2)
+		readinput();
+	else
+		sockclient();
 	return 0;
 }
