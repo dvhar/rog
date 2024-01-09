aba16a76a97d5ec8e5f8a7b81e5b3212018fb18b
dvhar <dh@protonmail.com>
2024-01-08 22:29:58 -0600

    args and fifo

--- rog/src/main.rs
+++ rog/src/main.rs
@@ -1,9 +1,14 @@
 use std::io::{self, Read, Write};
-use std::sync::mpsc::{channel,Receiver};
+use std::sync::mpsc::{channel,Receiver, Sender};
 use std::net::{ TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::process;
+use std::fs;
+extern crate getopts;
+extern crate unix_named_pipe;
+
+use getopts::Options;
 
 const BUFSZ: usize = 1024;
 
@@ -77,7 +82,7 @@ fn sock_serve(rb: Arc<Mutex<RingBuf>>, rx: Receiver<i32>) {
     let listener = match TcpListener::bind("0.0.0.0:19888") {
         Ok(lsnr) => lsnr,
         Err(_) => {
-            eprintln!("Could not bind to port. Is a rog server already running?");
+            eprintln!("Could not bind to port. Is a server already running?");
             process::exit(0);
         },
     };
@@ -87,7 +92,10 @@ fn sock_serve(rb: Arc<Mutex<RingBuf>>, rx: Receiver<i32>) {
         match stream {
             Ok(mut stream) => {
                 loop {
-                    rx.recv().unwrap();
+                    if let Err(e) = rx.recv() {
+                        eprintln!("recv error:{}", e);
+                        continue;
+                    }
                     let n = ringbuf.lock().unwrap().read_to(&mut buf);
                     if n == 0 {
                         continue;
@@ -102,20 +110,44 @@ fn sock_serve(rb: Arc<Mutex<RingBuf>>, rx: Receiver<i32>) {
     }
 }
 
-fn read_and_serve() {
+
+fn read_and_serve(input: Option<String>) {
     let rb = Arc::new(Mutex::new(RingBuf::new()));
     let rb_serv = rb.clone();
     let (tx, rx) = channel::<i32>();
     thread::spawn(move || sock_serve(rb_serv, rx));
+
+    if let Some(path) = input {
+        unix_named_pipe::create("rogfifo", Some(0o666)).expect("could not create fifo");
+        if let Err(e) = fs::rename("rogfifo",path.as_str()) {
+            eprintln!("Move fifo errro:{}",e);
+            process::exit(1);
+        }
+        loop {
+            let file = fs::File::open(path.as_str()).expect("could not open fifo for reading");
+            read_input(file, &tx, rb.clone());
+        }
+    } else {
+        read_input(io::stdin(), &tx, rb);
+    }
+}
+
+fn read_input<T: Read>(mut reader: T, tx: &Sender<i32>, rb: Arc<Mutex<RingBuf>>) {
     loop {
         let mut buf = [0u8; BUFSZ];
-        match io::stdin().read(&mut buf) {
+        match reader.read(&mut buf) {
             Ok(n) if n > 0 => {
                 rb.lock().unwrap().read_from(&buf[0..n]);
-                tx.send(0).unwrap();
+                if let Err(e) = tx.send(0) {
+                    eprintln!("send error:{}", e);
+                    continue;
+                }
             },
-            Ok(_) => {},
-            Err(_) => return,
+            Ok(_) => return,
+            Err(e) => {
+                eprintln!("read err:{}",e);
+                return;
+            }
         }
     }
 }
@@ -123,22 +155,46 @@ fn read_and_serve() {
 fn client(host: &str) {
     let mut stream = TcpStream::connect(format!("{}:19888",host)).expect("Connection failed");
     let mut buf = [0; BUFSZ];
-    stream.write(b"hi").unwrap();
     loop {
         match stream.read(&mut buf) {
             Ok(n) if n == 0 => return,
             Ok(n) => io::stdout().write(&buf[..n]).expect("Write failed"),
-            Err(_) => return,
+            Err(e) => {
+                eprintln!("error reading from stream:{}", e);
+                return;
+            },
         };
     }
 }
 
 fn main() {
-    let host = "127.0.0.1";
     let args: Vec<String> = std::env::args().collect();
-    if args.len() == 1 {
-        read_and_serve();
+    let mut opts = Options::new();
+    opts.optopt("i", "ip", "ip of server (for client)", "IP");
+    opts.optopt("f", "fifo", "path of fifo you want to create and read from", "PATH");
+    opts.optflag("s", "server", "run server mode");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(e) => {
+            eprintln!("bad args:{}", e);
+            process::exit(1);
+        },
+    };
+
+    if matches.opt_present("h") {
+        println!("rog
+    -h help
+    -i <ip> ip address of server
+    -s server mode
+    -f <path> server mode, but read from fifo instead of stdin");
+        process::exit(0);
+    }
+
+    if matches.opt_present("s") || matches.opt_present("f") {
+        read_and_serve(matches.opt_str("f"));
         return;
     }
-    client(host);
+    let host = matches.opt_str("i").unwrap_or("127.0.0.1".to_string());
+    client(host.as_str());
 }
