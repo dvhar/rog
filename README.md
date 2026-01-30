b86377b4b929bb15bbed265ba98572d2efa7e850
dvhar <dh22@protonmail.com>
2026-01-30 08:27:27 -0600

    refactor tests

---
+++ README.md
@@ -0,0 +1,20 @@
+Halfway between `tail -f` and `lnav`
+
+# Under construction...
+
+## static link
+```
+pacman -S musl
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl   
+```
+
+## test
+```
+cargo test --test cli
+```
+
+## debug logging
+```
+cargo build --features debug_log
+```
--- tests/cli.rs
+++ tests/cli.rs
@@ -8,41 +8,39 @@ const TEST_INPUT: &str = r#"2023-11-12 20:48:30.241 INFO [src/main.rs:13] Starti
 2023-11-12 20:48:30.244 INFO [src/main.rs:17] Done
 "#;
 
-#[test]
-fn test_no_args() {
-    let mut cmd = Command::new(env!("CARGO_BIN_EXE_rog")).arg("-c")
+fn run_test_stdin(args: &[&str], input: &str, expected_output: &str) {
+    let mut cmd = Command::new(env!("CARGO_BIN_EXE_rog"));
+    cmd.args(args)
         .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+        .stdout(Stdio::piped());
 
-    let stdin = cmd.stdin.as_mut().unwrap();
-    stdin.write_all(TEST_INPUT.as_bytes()).unwrap();
+    let mut child = cmd.spawn().unwrap();
+    let stdin = child.stdin.as_mut().unwrap();
+    stdin.write_all(input.as_bytes()).unwrap();
 
-    let output = cmd.wait_with_output().unwrap();
+    let output = child.wait_with_output().unwrap();
     assert!(output.status.success());
 
     let stdout = String::from_utf8(output.stdout).unwrap();
-    assert_eq!(stdout.trim(), TEST_INPUT.trim());
+    assert_eq!(stdout.trim(), expected_output.trim());
+}
+
+#[test]
+fn test_no_args() {
+    run_test_stdin(&["-c"], TEST_INPUT, TEST_INPUT);
 }
 
 #[test]
 fn test_simple_grep() {
-    let mut cmd = Command::new(env!("CARGO_BIN_EXE_rog")).arg("-c")
-        .arg("-g")
-        .arg("stuff")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    let stdin = cmd.stdin.as_mut().unwrap();
-    stdin.write_all(TEST_INPUT.as_bytes()).unwrap();
-
-    let output = cmd.wait_with_output().unwrap();
-    assert!(output.status.success());
-
-    let stdout = String::from_utf8(output.stdout).unwrap();
     let expected = r#"2023-11-12 20:48:30.242 INFO [src/main.rs:15] Doing stuff"#;
-    assert_eq!(stdout.trim(), expected.trim());
+    run_test_stdin(&["-c", "-g", "stuff"], TEST_INPUT, expected);
+}
+
+#[test]
+fn test_grep_bctx() {
+    let expected = r#"2023-11-12 20:48:30.241 WARN [src/main.rs:14] Just a warning
+2023-11-12 20:48:30.242 INFO [src/main.rs:15] Doing stuff
+2023-11-12 20:48:30.243 ERROR [src/main.rs:16] Oups, an error
+"#;
+    run_test_stdin(&["-c", "-g", "Oups", "-B2"], TEST_INPUT, expected);
 }
